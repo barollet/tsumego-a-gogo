2c6a6ccebd5c94b8a17e01087ed5a2053815ff58
Theo Barollet <theo.barollet@inria.fr>
2023-03-20 11:15:41 +0100

    removed frontend2 old frontend

--- frontend2/src/config.js
+++
@@ -1,9 +0,0 @@
-const axios = window.axios;
-
-export const axios_client = axios.create({
-   baseURL: "http://127.0.0.1:8000/core/" 
-});
-
-export const React = window.React;
-export const ReactRouter = window.ReactRouterDOM;
-export const JGO = window.JGO;
